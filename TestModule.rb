b01a5b157c0554fa6630467b12805655c8e24b85
John Odom <john.osborne.odom@gmail.com>
2020-01-06 16:18:02 -0500

    first commit

---
+++ TestModule.rb
@@ -0,0 +1,5 @@
+require_relative 'Toolmodule.rb'
+include Tools 
+
+Tools.hello('John')
+
---
+++ Toolmodule.rb
@@ -0,0 +1,8 @@
+module Tools
+    def hello(name)
+        puts "hi, #{name}"
+    end
+    def goodbye(name)
+        puts "bye, #{name}"
+    end
+end
